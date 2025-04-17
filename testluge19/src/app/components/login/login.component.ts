7cf9787240a987145ad1b4ba415f1beb494e1e19
niketh <niketh.techintl@gmail.com>
2025-04-17 15:25:41 +0530

    loginauth

--- testluge19/src/app/components/login/login.component.ts
+++ testluge19/src/app/components/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Authsevice } from '../../auth.service';
+import { ILogin } from '../../../Logininterface';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = ''
+constructor(private authService: Authsevice){}
+onLogin(){
+  const credentials = {
+    username: this.username,
+    password: this.password
+  }
+ this.authService.login(credentials).subscribe({
+  next: (res : ILogin) => {
+    console.log('resss',res)
+    if(res.accessToken){
+      alert('user identified')
+    }
+  },
+  error: (err) => {
+    console.log('error',err.message)
+  }
+ })
+}
 }
